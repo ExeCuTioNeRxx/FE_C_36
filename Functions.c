cc36bd72d5529b9d0e9e38725322209ec28a619a
ExeCuTioNeRxx <103025184+ExeCuTioNeRxx@users.noreply.github.com>
2022-04-12 11:52:56 +0530

    Add files via upload

---
+++ Functions.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void add ()
+{
+int a =1;
+int b =999;
+int c =a+b;
+printf("Sum of a and b is %d\n",c);
+}
+void main()
+{    
+    add();
+}
+
+
+//Output: Sum of a and b is 1000
