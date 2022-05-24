933ec4eda7c4b9f1341a364e81ee9ae26bc0896d
PratikkPcodes <103025184+PratikkPcodes@users.noreply.github.com>
2022-05-24 11:59:04 +0530

    Add files via upload

---
+++ string_length.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<string.h>
+int main() 
+{
+char arr[]="xyz";
+int len1=strlen(arr);
+    printf("string=%s length=%d\n",arr,len1);
+    
+    return 0;
+}
+
+// Output
+string=xyz length=3
