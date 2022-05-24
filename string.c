6e0fea26474b8f81bc8c46802a60c4ed2e361d0a
PratikkPcodes <103025184+PratikkPcodes@users.noreply.github.com>
2022-05-24 12:17:17 +0530

    Add files via upload

--- string.c
+++ string.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 #include<string.h>
 int main()
-{ 
-    char strname [40];
-    printf ("enter the string\n");
-    scanf("%[^\n]s" , strname);
-    printf ("the string is %s\n", strname);
+{
+    char strname[30];
+    printf("Enter the string\n");
+    scanf("%[^\n]s",strname);
+    printf("The string is %s\n",strname);
     return 0;
+
 }
-Output:
 /*
-enter the string
-enter the string
-the string is enter the string
+Output
+Enter the string
+not valid
+The string is not valid 
 */
